2c30e1fb34a78265440f64fc087e2fe2b050fee5
RAJKUMAR-E <113916123+RAJKUMAR-E@users.noreply.github.com>
2025-09-18 16:18:05 +0530

    Update DemoApplication.java

--- project/springboot-demo/main/java/demo/DemoApplication.java
+++ project/springboot-demo/main/java/demo/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,3 +10,4 @@ public class DemoApplication {
     System.out.println("✅ Spring Boot app is running!");
   }
 }
+
