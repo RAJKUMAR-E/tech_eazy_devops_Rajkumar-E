30981071cead38249618fd1a4549a15193385c1d
RAJKUMAR-E <113916123+RAJKUMAR-E@users.noreply.github.com>
2025-09-22 21:43:18 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -9,30 +9,12 @@ data "aws_key_pair" "loki_key" {
 resource "aws_instance" "loki_ec2" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  key_name      = data.aws_key_pair.loki_key.key_name
 
   tags = {
     Name = var.name_tag
   }
 }
 
-resource "aws_security_group" "ssh_access" {
-  name        = "loki-ssh-access"
-  description = "Allow SSH from GitHub Actions"
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
 
 
