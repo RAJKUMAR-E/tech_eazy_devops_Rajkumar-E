6cbb68539c15d4b3c667299760263fee769246e7
RAJKUMAR-E <113916123+RAJKUMAR-E@users.noreply.github.com>
2025-09-22 22:26:22 +0530

    Update outputs.tf

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -1,9 +1,10 @@
 output "instance_id" {
   description = "ID of the EC2 instance"
-  value       = aws_instance.loki_ec2.id
+  value       = aws_instance.web_server.id
 }
 
 output "public_ip" {
   description = "Public IP of the EC2 instance"
-  value       = aws_instance.loki_ec2.public_ip
+  value       = aws_instance.web_server.public_ip
 }
+
