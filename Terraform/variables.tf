1cc63c19eff2e70e8c7bda75a9874736e972aa7b
RAJKUMAR-E <113916123+RAJKUMAR-E@users.noreply.github.com>
2025-09-22 14:31:07 +0530

    Create variables.tf

---
+++ Terraform/variables.tf
@@ -0,0 +1,23 @@
+variable "aws_region" {
+  description = "AWS region to deploy EC2"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "ami_id" {
+  description = "AMI ID for EC2 instance"
+  type        = string
+  default     = "ami-0f5ee92e2d63afc18"  # Amazon Linux 2 in ap-south-1
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "name_tag" {
+  description = "Name tag for EC2 instance"
+  type        = string
+  default     = "loki-EC2"
+}
