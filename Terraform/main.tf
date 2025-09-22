54fbf1d29b293243819f9266f3429d3b6603d49c
RAJKUMAR-E <113916123+RAJKUMAR-E@users.noreply.github.com>
2025-09-22 22:33:34 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -28,9 +28,10 @@ data "aws_ami" "amazon_linux" {
   owners      = ["amazon"]
 
   filter {
-    name   = "name"
-    values = ["al2023-ami-*-kernel-6.1-x86_64"]
-  }
+  name   = "name"
+  values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+}
+
 
   filter {
     name   = "architecture"
