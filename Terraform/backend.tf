dae9aa03a765384f329056999caae8c01a8fad31
RAJKUMAR-E <113916123+RAJKUMAR-E@users.noreply.github.com>
2025-09-25 10:44:03 +0530

    Update backend.tf

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -3,8 +3,6 @@ terraform {
     address            = "https://api.tfstate.dev/app/RAJKUMAR-E/tech_eazy_devops_RAJKUMAR-E"
     lock_address       = "https://api.tfstate.dev/app/RAJKUMAR-E/tech_eazy_devops_RAJKUMAR-E/lock"
     unlock_address     = "https://api.tfstate.dev/app/RAJKUMAR-E/tech_eazy_devops_RAJKUMAR-E/unlock"
-    username           = "RAJKUMAR-E"
-    password           = "ghp_XXXXXXXXXXXXXXXXXXXXXXXXXXXX"
     skip_cert_verification = false
   }
 }
